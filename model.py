a8d6b7d22bc04a8b3e1bcee2d991165842626d91
manujshinkar <shinkarmanuj@gmail.com>
2017-04-17 15:15:45 -0700

    Update model.py

--- model.py
+++ model.py
@@ -1,87 +1,95 @@
+import os
 import csv
 import cv2
 import numpy as np
-import matplotlib.pyplot as plt
+import sklearn
+from sklearn.utils import shuffle
 
-
-def preprocess_image(img):
-	new_img = img[50:140,:,:]
-	new_img = cv2.resize(img,(200, 66), interpolation = cv2.INTER_AREA)
-	return new_img
-
-lines = []
+samples = []
+i = 0
 with open('data/driving_log.csv') as csvfile:
-	reader = csv.reader(csvfile)
-	for line in reader:
-		lines.append(line)
-
-images = []
-measurements = []
-steering_correction = 0.25
-for line in lines[1:]:
-	for i in range(3):
-		source_path = line[i]
-		filename = source_path.split('/')[-1]
-		current_path = 'data/IMG/' + filename
-		image = cv2.imread(current_path)
-		#processed_image = preprocess_image(image)
-		images.append(image)
-		measurement = float(line[3])
-		if i == 1:
-			 measurement += steering_correction
-		elif i == 2:
-			 measurement -= steering_correction
-		measurements.append(measurement)
-
-augmented_images, augmented_measurements = [], []
-for image, measurement in zip(images, measurements):
-	augmented_images.append(image)
-	augmented_measurements.append(measurement)
-	augmented_images.append(cv2.flip(image,1))
-	augmented_measurements.append(measurement*-1.0)
+    reader = csv.reader(csvfile)
+    for line in reader:
+        if i > 0 :
+            samples.append(line)
+        i = i+1
 	
-X_train = np.array(augmented_images)
-y_train = np.array(augmented_measurements)
+def preprocess_image(img):
+    new_img = img[50:140,:,:]
+    new_img = cv2.resize(new_img ,(200, 66), interpolation = cv2.INTER_AREA)
+    new_img = cv2.cvtColor(new_img, cv2.COLOR_BGR2YUV)
+    return new_img
 
-#plt.figure(figsize=(160, 320))
-#plt.imshow(images[4])
-#plt.show()
+from sklearn.model_selection import train_test_split
+train_samples, validation_samples= train_test_split(samples, test_size=0.2)
+
+def generator(samples, batch_size=32):
+    num_samples = len(samples)
+    while 1: # Loop forever so the generator never terminates
+        small_batch_size = 32
+        shuffle(samples)
+        for offset in range(0, num_samples, small_batch_size):
+            batch_samples = samples[offset:offset+small_batch_size]
+
+            images = []
+            measurements = []
+            for batch_sample in batch_samples:
+
+                for i in range(3):
+                    source_path = './data/IMG/'+batch_sample[i]
+                    filename = source_path.split('/')[-1]
+                    current_path = 'data/IMG/' + filename
+                    image = cv2.imread(current_path)
+                    processed_image = preprocess_image(image)
+                    steering_correction = 0.25
+                    measurement = float(batch_sample[3])
+                    if i == 1:
+                         measurement += steering_correction
+                    elif i == 2:
+                         measurement -= steering_correction
+
+                    images.append(processed_image)
+                    measurements.append(measurement)
+                    images.append(cv2.flip(processed_image,1))
+                    measurements.append(measurement*-1.0)
+
+            X_train = np.array(images)
+            y_train = np.array(measurements)
+
+            yield sklearn.utils.shuffle(X_train, y_train)
+	
+# compile and train the model using the generator function
+train_generator = generator(train_samples, batch_size= 192)
+validation_generator = generator( validation_samples, batch_size=192)
 
 from keras.models import Sequential
-from keras.layers import Flatten, Dense, Lambda 
+from keras.layers import Flatten, Dense, Lambda, Dropout 
 from keras.layers.convolutional import Convolution2D, Cropping2D
 from keras.layers.pooling import MaxPooling2D
-#from keras.layers.cropping import Cropping2D
+from keras.layers.advanced_activations import ELU
+from keras.callbacks import ModelCheckpoint, Callback
+from keras.regularizers import l2
 
 model = Sequential()
-model.add(Lambda(lambda x: x / 127.5 - 1.0, input_shape=(160,320,3)))
-model.add(Cropping2D(cropping=((70,25), (0,0))))
-model.add(Convolution2D(24, 5, 5,subsample = (2,2), activation="relu"))
-model.add(Convolution2D(36, 5, 5,subsample = (2,2), activation="relu"))
-model.add(Convolution2D(48, 5, 5,subsample = (2,2), activation="relu"))
-model.add(Convolution2D(64, 3, 3, activation="relu"))
-model.add(Convolution2D(64, 3, 3, activation="relu"))
+model.add(Lambda(lambda x: x / 127.5 - 1.0, input_shape=(66,200,3)))
+#model.add(Cropping2D(cropping=((70,25), (0,0))))
+model.add(Convolution2D(24, 5, 5,subsample = (2,2), border_mode='valid', activation="elu", W_regularizer=l2(0.001)))
+model.add(Convolution2D(36, 5, 5,subsample = (2,2), border_mode='valid', activation="elu", W_regularizer=l2(0.001)))
+model.add(Convolution2D(48, 5, 5,subsample = (2,2), border_mode='valid', activation="elu" , W_regularizer=l2(0.001)))
+model.add(Convolution2D(64, 3, 3, border_mode='valid', activation="elu", W_regularizer=l2(0.001)))
+model.add(Convolution2D(64, 3, 3, border_mode='valid', activation="elu", W_regularizer=l2(0.001)))
+
 model.add(Flatten())
-model.add(Dense(100))
-model.add(Dense(50))
-model.add(Dense(10))
+
+model.add(Dense(100, activation="elu", W_regularizer=l2(0.001)))
+model.add(Dense(50, activation="elu", W_regularizer=l2(0.001)))
+model.add(Dense(10, activation="elu", W_regularizer=l2(0.001)))
 model.add(Dense(1))
 
-
-
-'''
-model = Sequential()
-model.add(Cropping2D(cropping=((50,20), (0,0)), input_shape=(160,320,3)))
-model.add(Lambda(lambda x: x / 255.0 - 0.5, input_shape=(160,320,3)))
-model.add(Convolution2D(6, 5, 5, activation="relu"))
-model.add(MaxPooling2D())
-model.add(Convolution2D(6, 5, 5, activation="relu"))model.add(MaxPooling2D())
-model.add(Flatten())
-model.add(Dense(120))
-model.add(Dense(84))
-model.add(Dense(1))
-'''
 model.compile(loss='mse', optimizer='adam')
-model.fit(X_train, y_train, validation_split=0.2, shuffle=True, nb_epoch=2)
+checkpoint = ModelCheckpoint('model{epoch:02d}.h5')
+model.fit_generator(train_generator, samples_per_epoch= len(train_samples) * 6,
+                    validation_data=validation_generator, nb_val_samples=len(validation_samples)*6,
+                    nb_epoch = 5, callbacks=[checkpoint])
 
 model.save('model.h5') 
